96aaa216a2e7eb754f1f7852dc13fd787a911b11
kiplangatbryan <kiplangatsgt@gmail.com>
2024-03-17 06:56:44 -0700

    fixed configuration prettier

--- src/components/GetAQuoteModal.tsx
+++ src/components/GetAQuoteModal.tsx
@@ -122,7 +122,6 @@ export default function GetAQuoteModal({ open, setOpen }: SliderOverProp) {
                             </div>
                           </div>
 
-
                           <div>
                             <button
                               type='submit'
