f4b814af7dd08359230a374e848efb89ade5a15a
kiplangatbryan <kiplangatsgt@gmail.com>
2024-04-15 08:04:53 -0700

    adding new look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 
-import { Inter } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
 import './globals.css';
 import type { Metadata } from 'next';
 
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     'VerbalScripts offers fast, affordable, and highly accurate human transcription, translation, data annotation, and AI dataset services customized for your needs.',
 };
 
-const inter = Inter({ subsets: ['latin'] });
+const dm_sans = DM_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className={inter.className}>
+    <html lang='en' className={dm_sans.className}>
       <body className='relative min-h-screen'>{children}</body>
     </html>
   );
