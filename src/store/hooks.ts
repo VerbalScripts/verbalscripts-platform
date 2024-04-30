63e609af5dd5095e1a98b515a804a950683ae340
kiplangatbryan <kiplangatsgt@gmail.com>
2024-04-30 15:22:05 -0700

    swish

--- src/store/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector, useStore } from 'react-redux';
-
-import type { AppDispatch, AppStore, RootState } from './configureStore';
-
-// use modified hooks
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
