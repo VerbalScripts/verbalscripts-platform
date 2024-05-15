67166ed4933ba62b0e9e5d07423b81c265289862
kiplangatbryan <kiplangatsgt@gmail.com>
2024-05-15 10:22:15 -0700

    medical plus

--- src/app/services/medical/page.tsx
+++ src/app/services/medical/page.tsx
@@ -18,144 +18,44 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  const audio_formats = [
-    {
-      name: 'aac',
-      icon: '/services/aac.png',
-    },
-    {
-      name: 'flac',
-      icon: '/services/flac.png',
-    },
-    {
-      name: 'm3u',
-      icon: '/services/m3u.png',
-    },
-    {
-      name: 'mp4',
-      icon: '/services/audio-mp4.png',
-    },
-    {
-      name: 'amr',
-      icon: '/services/amr.png',
-    },
-    {
-      name: 'wmv',
-      icon: '/services/wmv.png',
-    },
-    {
-      name: 'asf',
-      icon: '/services/asf.png',
-    },
-    {
-      name: 'ogg',
-      icon: '/services/ogg.png',
-    },
-    {
-      name: 'mpg',
-      icon: '/services/mpg.png',
-    },
-    {
-      name: 'webm',
-      icon: '/services/webm.png',
-    },
-    {
-      name: 'pls',
-      icon: '/services/pls.png',
-    },
-    {
-      name: 'rtf',
-      icon: '/services/rtf.png',
-    },
-    {
-      name: 'm4a',
-      icon: '/services/m4a.png',
-    },
-  ];
 
-  const video_formats = [
-    {
-      name: 'mp4',
-      icon: '/services/video-mp4.png',
-    },
-    {
-      name: 'avi',
-      icon: '/services/avi.png',
-    },
-    {
-      name: 'mov',
-      icon: '/services/mov.png',
-    },
-    {
-      name: 'asf',
-      icon: '/services/asf.png',
-    },
-    {
-      name: 'amr',
-      icon: '/services/amr.png',
-    },
-    {
-      name: 'wmv',
-      icon: '/services/wmv.png',
-    },
-    {
-      name: 'ogg',
-      icon: '/services/ogg.png',
-    },
-    {
-      name: 'mpg',
-      icon: '/services/mpg.png',
-    },
-    {
-      name: 'flv',
-      icon: '/services/flv.png',
-    },
-    {
-      name: 'amr',
-      icon: '/services/amr.png',
-    },
-    {
-      name: 'wav',
-      icon: '/services/wav.png',
-    },
-  ];
 
   const audio_video_theme = [
     {
       imgUrl: '/svg/podcast.svg',
-      title: 'Meeting',
+      title: 'Patient Notes',
       description:
-        'From lively Zoom, Microsoft Teams or Google Meets brainstorming sessions to productive conference calls, we capture every detail for seamless collaboration.',
+        'Capture patient encounters with precision, ensuring comprehensive documentation of medical histories and treatment plans.',
     },
     {
       imgUrl: '',
-      title: 'Podcasts',
+      title: 'Operative Reports',
       description:
-        'Turn your captivating podcast episodes from Spotify, Apple Podcasts, or Google Podcasts into searchable text, expanding your audience reach and boosting SEO.',
+        'Detail surgical procedures with clarity, facilitating seamless communication among healthcare providers and accurate patient care.',
     },
     {
       imgUrl: '',
-      title: 'Seminars & Webinars',
+      title: 'Consultation Notes',
       description:
-        'Preserve the knowledge shared in dynamic webinars and seminars conducted on platforms like GoToWebinar or Zoom Webinar, making insights accessible long after the event ends.',
+        'Record detailed assessments and recommendations from healthcare professionals, aiding in continuity of care and patient follow-up.',
     },
     {
       imgUrl: '',
-      title: 'Video & Voice Calls',
+      title: 'Discharge Summaries',
       description:
-        'Capture important conversations from any platform, whether it"s a client meeting on Skype or a brainstorming session on Microsoft Teams, for future reference.',
+        'Summarize patient treatment and post-discharge instructions effectively, enhancing patient understanding and compliance.',
     },
     {
       imgUrl: '',
-      title: 'Sermons',
+      title: 'Progress Reports',
       description:
-        'Spread inspiration with written versions of your powerful sermons from church recordings, to online streaming platforms, reaching a broader audience and fostering deeper reflection.',
+        'Document patient progress over time, providing valuable insights for ongoing treatment adjustments and healthcare management.',
     },
     {
       imgUrl: '',
-      title: 'Testimonials',
+      title: 'Medical History and Physical Exams',
       description:
-        'Showcase the satisfaction of your clients by transforming heartfelt testimonials from video, audio, or written form into compelling written content.',
+        'Preserve detailed medical histories and examination findings, supporting comprehensive patient evaluations and treatment planning.',
     },
 
     {
@@ -172,28 +72,28 @@ export default function Page() {
     },
   ];
 
-  const audio_features = [
-    {
-      title: 'Enhanced Accessibility',
-      description:
-        'Make your content accessible to a wider audience, including those with hearing impairments and non-native speakers, improving inclusivity and engagement.',
-    },
-    {
-      title: 'Content Purposing',
-      description:
-        ' Repurpose transcribed content into various formats for content marketing strategies, extending the reach of your original content.',
-    },
-    {
-      title: 'Professional Image',
-      description:
-        'Enhance your brand image and professionalism by delivering high-quality transcriptions to your audience.',
-    },
-    {
-      title: 'Time Savings',
-      description:
-        ' Save time and resources by outsourcing your transcription needs to professionals with quick turnaround times.',
-    },
-  ];
+  // const audio_features = [
+  //   {
+  //     title: 'Enhanced Accessibility',
+  //     description:
+  //       'Make your content accessible to a wider audience, including those with hearing impairments and non-native speakers, improving inclusivity and engagement.',
+  //   },
+  //   {
+  //     title: 'Content Purposing',
+  //     description:
+  //       ' Repurpose transcribed content into various formats for content marketing strategies, extending the reach of your original content.',
+  //   },
+  //   {
+  //     title: 'Professional Image',
+  //     description:
+  //       'Enhance your brand image and professionalism by delivering high-quality transcriptions to your audience.',
+  //   },
+  //   {
+  //     title: 'Time Savings',
+  //     description:
+  //       ' Save time and resources by outsourcing your transcription needs to professionals with quick turnaround times.',
+  //   },
+  // ];
   // const features = [
   //   {
   //     name: 'First Draft',
@@ -248,12 +148,14 @@ export default function Page() {
         <div className='flex flex-wrap  md:flex-nowrap items-center gap-x-10 '>
           <div className='max-w-2xl md:max-w-3xl'>
             <h1 className='text-4xl font-bold capitalize tracking-tight text-gray-800 sm:text-5xl'>
-            Medical Transcription
+              Medical Transcription
             </h1>
 
             <p className='block md:max-w-2xl lg:max-w-3xl xl:max-w-4xl relative z-10 mt-6 text-lg  leading-7 text-gray-800'>
-            Welcome to VerbalScripts{'"'} Medical Transcription Services! We know how crucial accurate medical records are. With our dedicated team, expect precise transcriptions while we keep your information secure and confidential.
-
+              Welcome to VerbalScripts{'"'} Medical Transcription Services! We
+              know how crucial accurate medical records are. With our dedicated
+              team, expect precise transcriptions while we keep your information
+              secure and confidential.
             </p>
 
             {/* <div className='mt-10'>
@@ -277,7 +179,8 @@ export default function Page() {
                   Human Transcripted
                 </div>
                 <div className='text-gray-500 font-semibold mb-3'>
-                  From <span className='footer-title'>$1</span> per minute 98% accuracy
+                  From <span className='footer-title'>$1</span> per minute 98%
+                  accuracy
                 </div>
               </div>
               <div className='px-4'>
@@ -315,114 +218,57 @@ export default function Page() {
 
       <div className='py-10 md:py-16' style={{ background: '#FAFAFA' }}>
         <div className='mx-auto max-w-7xl px-6 md:px-16 lg:px-16 xl:px-28'>
-          <div className='flex flex-wrap md:flex-nowrap items-center gap-x-6'>
-            <div className='md:min-w-[25rem] lg:min-w-[30rem] text-left'>
+          <div className='flex flex-wrap md:flex-nowrap items-center space-x-6'>
+            <div className='order-2 md:min-w-[25rem] lg:min-w-[35rem] text-left'>
               <h2 className='text-xl font-bold leading-7 capitalize secondary-text'>
-                Audio & Video Transcription Services?
+                Medical Transcription Services?
               </h2>
               <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                We offer <br></br>{' '}
+                Tailored Medical <br></br> Transcription Solutions{' '}
                 <span className='relative z-10'>
-                  Comprehensive.{' '}
+                  Designed.{' '}
                   <span className='z-10 absolute -bottom-5 right-0'>
                     <img src='/svg/lines.svg' className='w-44' alt='' />
                   </span>
                 </span>{' '}
-                <br></br> transcription services <br></br> tailored to your
-                needs.
+                for you.
               </p>
-            </div>
-            <div>
-              <p className='mx-auto max-w-2xl mt-6 text-lg leading-8 text-gray-600'>
-                Looking for accuracy, efficiency, and a friendly team? Look no
-                further. With VerbalScripts, you{"'"}re not just getting
-                transcriptions; you{"'"}re getting peace of mind. Our
-                professionals ensure every detail is captured, every time.
-              </p>
+
+              <div>
+                <ul className='mx-auto max-w-2xl mt-6 text-lg leading-8 text-gray-800 list-disc flex flex-col space-y-2'>
+                  <li>
+                    From patient notes to operative reports, we transcribe
+                    medical dictations with precision.
+                  </li>
+                  <li>
+                    Our transcriptionists are well-versed in medical terminology
+                    and adhere to industry standards.
+                  </li>
+                  <li>
+                    Your patients{'"'} privacy is our priority. We strictly
+                    comply with HIPAA regulations and maintain confidentiality.
+                  </li>
+                  <li>
+                    Tailor our services to your specific needs, including
+                    formatting preferences and turnaround times.
+                  </li>
+                </ul>
+              </div>
               <div className='mt-10'>
                 <a
                   href='/upload-files'
                   className='inline-flex  items-center gap-x-2 rounded-lg btn-vlg px-8 py-2.5 text-xl transition font-semibold text-white shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 btn-vlg'
                 >
-                  <span>Explore Services</span>
+                  <span>Order Now</span>
                 </a>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='py-10 md:py-16 landing-page-alt '>
-        <div className='mx-auto max-w-7xl px-6 md:px-16 lg:px-16 xl:px-28'>
-          <div className='flex gap-y-10 bg-white rounded-xl px-8 py-10 flex-wrap md:flex-nowrap items-start justify-between gap-x-6 mb-20'>
-            <div className='md:min-w-[25rem] lg:min-w-[30rem] text-left'>
-              <div className='flex flex-col gap-y-10'>
-                <div>
-                  <h2 className='text-xl font-bold leading-7 capitalize secondary-text'>
-                    Supported audio formats?
-                  </h2>
-                  <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                    Audio Formats <br></br>that We Offer for
-                  </p>
-                </div>
-                <div>
-                  <h2 className='text-xl font-bold leading-7 capitalize secondary-text'>
-                    Unupported Audio formats?
-                  </h2>
-                  <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                    We’ll Assist You
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className='grid justify-evenly grid-cols-4 md:grid-cols-3   lg:gap-x-8 gap-y-5  lg:grid-cols-4 lg:gap-y-5  pb-12'>
-                {audio_formats.map((audio_format) => (
-                  <div key={audio_format.name} className=''>
-                    <img
-                      src={audio_format.icon}
-                      className='w-[4rem]'
-                      alt={audio_format.name}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
-          <div className='flex gap-y-10 bg-white  rounded-xl px-8 py-10  flex-wrap md:flex-nowrap items-start justify-between gap-x-6 mb-10'>
-            <div className='md:min-w-[25rem] lg:min-w-[30rem] text-left'>
-              <div className='flex flex-col gap-y-10'>
-                <div>
-                  <h2 className='text-xl font-bold leading-7 capitalize secondary-text'>
-                    Supported video formats?
-                  </h2>
-                  <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                    Video Formats <br></br>that We Offer for
-                  </p>
-                </div>
-                <div>
-                  <h2 className='text-xl font-bold leading-7 capitalize secondary-text'>
-                    Unupported video formats?
-                  </h2>
-                  <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                    We’ll Assist You
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className='grid justify-evenly grid-cols-4 md:grid-cols-3   lg:gap-x-8 gap-y-5  lg:grid-cols-4 lg:gap-y-5  pb-12'>
-                {video_formats.map((audio_format) => (
-                  <div key={audio_format.name} className=''>
-                    <img
-                      src={audio_format.icon}
-                      className='w-[4rem]'
-                      alt={audio_format.name}
-                    />
-                  </div>
-                ))}
-              </div>
+            <div className='order-1 hidden md:block relative w-full'>
+              <img
+                src='/pics/598513718_huge.jpg'
+                className='absolute -top-32 -left-10 w-[20rem] md:w-[28rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10'
+                alt=''
+              />
             </div>
           </div>
         </div>
@@ -475,6 +321,7 @@ export default function Page() {
           </div>
         </div>
       </div>
+
       <div className='py-10 md:py-16 why-section-bg '>
         <div className='mx-auto max-w-7xl    px-6 md:px-16 lg:px-16 xl:px-28'>
           <div className='flex flex-wrap md:flex-nowrap items-center gap-x-6 rounded-md md:px-8 md:py-10 bg-white/30 ring-1 ring-white/10'>
@@ -483,66 +330,29 @@ export default function Page() {
                 Why choose us?
               </h2>
               <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-                We take pride in offering comprehensive Audio & Video
-                Transcription Services for You
+                Rest assured, your medical transcription needs are in capable
+                hands.
               </p>
             </div>
             <div>
               <ul className='mx-auto max-w-2xl mt-6 text-lg leading-8 text-gray-800 list-disc flex flex-col gap-y-5'>
                 <li>
-                  Our experienced transcriptionists are skilled in accurately
-                  transcribing diverse audio and video content.
+                  Our team possesses in-depth knowledge of medical terminology,
+                  ensuring accurate transcriptions.
                 </li>
                 <li>
-                  We utilize streamlined processes to ensure efficiency and
-                  quick turnaround times without compromising quality.
+                  We adhere to stringent HIPAA regulations, ensuring the utmost
+                  confidentiality of patient information.
                 </li>
                 <li>
-                  We offer customizable options to meet your specific
-                  requirements, including formatting preferences and turnaround
-                  times.
+                  Your data is safe with us. We employ robust security measures
+                  to protect sensitive information.
                 </li>
                 <li>
-                  We prioritize confidentiality and security, ensuring that your
-                  content is handled with the utmost care.
+                  Receive dedicated customer support throughout your
+                  transcription journey.
                 </li>
               </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='py-10 md:py-24' style={{ background: '#FAFAFA' }}>
-        <div className='mx-auto max-w-7xl px-6 md:px-16 lg:px-16 xl:px-28'>
-          <div>
-            <h2 className='text-xl font-bold leading-7 capitalize text-indigo-600'>
-              Audio & Video Transcription Services?
-            </h2>
-            <p className='mt-2 text-4xl md:text-5xl  font-bold text-left text-gray-900 section-title'>
-              Empowering Your Audio & Video, Every Step
-            </p>
-          </div>
-          <div className='relative my-5'>
-            <img
-              src='/pics/78.png'
-              className='rounded-xl object-cover w-full h-[15rem]'
-              alt=''
-            />
-          </div>
-
-          <div className='my-10'>
-            <div className='grid max-w-none  grid-cols-1 md:grid-cols-2 gap-10 md:gap-16 lg:grid-cols-2 lg:gap-20'>
-              {audio_features.map((feature, index) => (
-                <div key={index} className='shadow-md rounded-xl py-10 px-12'>
-                  <div className='footer-title text-xl md:text-2xl font-semibold mb-3'>
-                    {feature.title}
-                  </div>
-
-                  <div className='text-gray-600 text-md'>
-                    {feature.description}
-                  </div>
-                </div>
-              ))}
             </div>
           </div>
         </div>
