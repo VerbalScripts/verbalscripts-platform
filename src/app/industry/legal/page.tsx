6723d379b7ea383777e566e28e0763cedf0eb959
kiplangatbryan <kiplangatsgt@gmail.com>
2024-06-23 18:39:56 +0300

    polish

--- src/app/industry/legal/page.tsx
+++ src/app/industry/legal/page.tsx
@@ -11,7 +11,6 @@ import PopularPartner from '@/components/PopularPartner';
 import HowItWorks from '@/components/HowItWorks';
 import Faqs from '@/components/Faqs';
 import { classNames } from '@/utils/classNames';
-import CallToActionBanner from '../../../components/CallToActionBanner';
 
 export const metadata: Metadata = {
   title: 'VerbalScripts | Court and Legal Reporting',
@@ -302,6 +301,48 @@ export default function Page() {
         </div>
       </div>
 
+      
+      <HowItWorks custom_class='legal-how-it-works' />
+
+    
+      <div
+        className='bg-white relative isolate'
+        style={{ background: 'FAFAFA' }}
+      >
+        <div className='relative overflow-hidden transition transform-gpu'>
+          <div className='mx-auto max-w-7xl px-6 lg:px-8 xl:px-14'>
+            <div className='relative isolate py-24   px-6   sm:px-16 md:py-10  lg:flex lg:items-center lg:gap-x-20 lg:px-24 lg:pt-0'>
+              <div className='md:min-w-[28rem] lg:min-w-[30rem]  lg:mx-0 lg:flex-auto lg:py-24 lg:text-left'>
+                <h2 className='text-4xl md:text-[2.8rem] font-bold leading-10 section-title  text-gray-800 '>
+                  Ready to Enhance Your Legal and Court Reporting Today with Us?
+                </h2>
+                <p className='mt-6  text-xl leading-8 text-gray-600'>
+                  Contact us today to discuss your requirements and discover how
+                  our specialized transcription solutions can benefit your legal
+                  practice.
+                </p>
+
+                <div className='mt-10 flex items-center flex-wrap justify-center gap-6 lg:justify-start'>
+                  <a
+                    href='/contact-us'
+                    className='rounded-xl btn-vlg px-7 py-2.5 text-lg font-semibold text-white transition focus:ring-4 focus:ring-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+                  >
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+              <div className='hidden lg:block relative mt-16 h-80 lg:-mt-8 pr-10'>
+                <img
+                  className='lg:w-[28rem] xl:w-[32rem] max-w-none'
+                  src='/legal_industry/contact.png'
+                  alt='Ready to Succeed with our transcription service'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className=' py-14 sm:py-12 bg-gray-100'>
         <div className='mx-auto max-w-7xl relative'>
           <div className='mx-auto px-6 md:px-0 md:max-w-2xl text-left lg:text-center'>
@@ -320,9 +361,6 @@ export default function Page() {
         </div>
       </div>
 
-      <HowItWorks custom_class='legal-how-it-works' />
-
-      <CallToActionBanner />
 
       <AppFooter />
     </div>
