fdafa7a4d1cad9b418add191e444df044f0614da
kiplangatbryan <kiplangatsgt@gmail.com>
2024-07-21 10:11:21 +0300

    alpine

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // export const hostUrl = 'https://verbal-backend.onrender.com';
 // export const hostUrl = 'http://localhost:7000';
 // hush
-export const hostUrl = 'https://service.verbalscripts.com';
+export const hostUrl = 'https://service.verbalscripts.com';``
 // export const hostUrl = 'https://kzbsm321-7000.euw.devtunnels.ms/';
